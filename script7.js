bea4c5ecf1abd51c8d749e71f8f39ec87d43da9b
Rychames <122337054+Rychames@users.noreply.github.com>
2023-07-26 21:57:17 -0400

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,25 @@
+function handleCredentialResponse(response) {
+  const data = jwt_decode(response.credential)
+  console.log(data)
+  }
+  window.onload = function () {
+    google.accounts.id.initialize({
+      client_id: "722146277894-mi0m4rs0lgjmchmjc4j7jcd7g19okla3.apps.googleusercontent.com",
+      callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline",
+       type:"standard",
+       shape:"pill",
+       theme:"filled_black",
+       text:"signin_with",
+       size:"large",
+       logo_alignment:"left"
+    
+    
+    
+    }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  }
